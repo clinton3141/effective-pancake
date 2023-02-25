0dae51674a095258e52fd9c609ea4d2bff34c8cb
Clinton Montague <iblamefish@gmail.com>
2023-02-25 17:26:02 +0000

    feat(list-3): views for deleting item. includes updates to make other views visually consistent

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,11 +7,24 @@
 @if (count($shoppingList) === 0)
     <p>Your shopping list is empty</p>
     @else
-    <ul role="list" class="max-w-md marker:text-red-400 list-disc pl-5 space-y-3">
+    <ul role="list" class="max-w-md marker:text-red-400 pt-5 pb-5 list-disc pl-5 space-y-3">
         @foreach ($shoppingList as $item)
-            <li>{{ $item->name }}</li>
+            <li class="mt-0">
+                <div class="flex">
+                    <div class="flex-auto">
+                        {{ $item->name }}
+                    </div>
+                    <div class="self-end">
+                        <form action="/v1/item/{{ $item->id }}" method="POST" onsubmit="javascript:return confirm('Delete &quot;{{ $item->name }}&quot;?')">
+                            @method('DELETE')
+                            @csrf
+                            <input type="submit" value="x" class="bg-red-200 rounded-full pl-2 pr-2 pb-1 cursor-pointer hover:bg-red-400" title="Delete &quot;{{ $item->name }}&quot;" />
+                        </form>
+                    </div>
+                </div>
+            </li>
         @endforeach
     </ul>
     @endif
-    <p><a href="/item">Add an item</a></p>
+    <p><a class="rounded-lg p-2 bg-blue-200 hover:bg-blue-400 cursor-pointer" href="/item">Add an item</a></p>
 @endsection

627de5a453b59b288addac0848a8c54789b1ae49
Clinton Montague <iblamefish@gmail.com>
2023-02-25 10:44:56 +0000

    feat(list-2): add item to list repo

--- app/Repositories/ListRepository.php
+++ app/Repositories/ListRepository.php
@@ -7,10 +7,19 @@ use Illuminate\Contracts\Session\Session;
 class ListRepository {
     const SESSION_TAG = 'shoppingList';
 
-    function __construct(private Session $session) {
+    function __construct(private Session $session) {}
 
-    }
-    public function getAll(): array {
+    public function getAll(): array
+    {
         return $this->session->get(self::SESSION_TAG) ?? [];
+    }
+
+    public function add(string $item): void
+    {
+        $shoppingList = $this->getAll();
+
+        array_push($shoppingList, $item);
+
+        $this->session->put(self::SESSION_TAG, $shoppingList);
     }
 }

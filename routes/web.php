7d961f73735227d42cc997342c84daf84c22296a
Clinton Montague <iblamefish@gmail.com>
2023-02-25 10:46:08 +0000

    feat(list-2): add item to list controller

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [ShoppingListController::class, 'showAll']);
+
+Route::view('/item', 'add-item');
+Route::post('/v1/item', [ShoppingListController::class, 'add']);
--- tests/Unit/ShoppingListControllerTest.php
+++ tests/Unit/ShoppingListControllerTest.php
@@ -21,4 +21,20 @@ class ShoppingListControllerTest extends TestCase
         $this->call('GET', '/')
             ->assertViewHas('shoppingList', ['Cherries']);
     }
+
+    public function test_controller_adds_item_to_shopping_list(): void
+    {
+        $this->instance(
+            ListRepository::class,
+            Mockery::mock(ListRepository::class, function (MockInterface $mock) {
+                $mock->shouldReceive('add')->once('Sugar');
+                $mock->shouldReceive('getAll')->andReturns(['Sugar']);
+            })
+        );
+
+        $this->followingRedirects();
+
+        $this->call('POST', '/v1/item', ['name' => 'Sugar'])
+            ->assertSee(['Sugar']);
+    }
 }

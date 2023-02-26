3ecbae403c03a9c0996ae4b1283334b1394f685b
Clinton Montague <iblamefish@gmail.com>
2023-02-26 11:36:34 +0000

    feat(list-6): migration to add order to list model

--- app/Models/ShoppingListItem.php
+++ app/Models/ShoppingListItem.php
@@ -22,5 +22,5 @@ class ShoppingListItem extends Model
         'isBought' => false,
     ];
 
-    protected $fillable = ['name', 'isBought'];
+    protected $fillable = ['name', 'isBought', 'order'];
 }
